f76368023e85dd53f69052f0d789a286122b7c44
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-10-28 01:23:54 +0100

    BS added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello App Week 5</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>App Week 5</h1>
+        <p>
+          Created by Viktoriia Perehuda,{" "}
+          <a
+            href="https://github.com/tourdesegur/app-react-week5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
