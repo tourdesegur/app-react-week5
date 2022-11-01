b64e0f57d5d851653ca5523d83cc2d03a4766805
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-11-01 17:24:29 +0000

    Fixed loop warning

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,6 +28,8 @@ export default function WeatherForecast(props) {
                   <ForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
